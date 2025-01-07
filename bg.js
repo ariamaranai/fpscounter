f1ff89396e716dc5599528252de2420e2626e1e4
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-01-07 23:16:31 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,12 +1,14 @@
 chrome.action.onClicked.addListener(async tab => {
-  let tabId = tab.id;
-  let target = { tabId };
-  (await chrome.debugger.getTargets()).find(v => v.tabId == tabId).attached
-    ? chrome.debugger.detach(target)
-    : (
-      chrome.debugger.attach(target, "1.3"),
-      chrome.debugger.sendCommand(target, "DOM.enable"),
-      chrome.debugger.sendCommand(target, "Overlay.enable"),
-      chrome.debugger.sendCommand(target, "Overlay.setShowFPSCounter", { show: !0 })
-    );
+  if (tab.url[0] != "c") {
+    let tabId = tab.id;
+    let target = { tabId };
+    (await chrome.debugger.getTargets()).find(v => v.tabId == tabId).attached
+      ? chrome.debugger.detach(target)
+      : (
+        chrome.debugger.attach(target, "1.3"),
+        chrome.debugger.sendCommand(target, "DOM.enable"),
+        chrome.debugger.sendCommand(target, "Overlay.enable"),
+        chrome.debugger.sendCommand(target, "Overlay.setShowFPSCounter", { show: !0 })
+      );
+  }
 });
